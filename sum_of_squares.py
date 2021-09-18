6dd4f507e080b6b70727a0ab87d15cbde246992f
Michal1898 <michal.volf@gmail.com>
2021-09-18 11:15:53 +0200

    first version - for testing purposes.

---
+++ sum_of_squares.py
@@ -0,0 +1,27 @@
+
+def sum_of_squares(n):
+    list_of_squares = []
+    for side_of_square in range(1,n):
+        square_volume = pow(side_of_square, 2)
+        if (square_volume<n):
+            list_of_squares.append(square_volume)
+        else:
+            break
+    # couples of volume search
+    list_of_couples_squares=[]
+    for first_square in list_of_squares:
+        for second_square in list_of_squares:
+            if (first_square+second_square)==n:
+                couple_of_squares=(first_square, second_square)
+                list_of_couples_squares.append(couple_of_squares)
+
+    if len(list_of_couples_squares):
+        for pair_of_squares in list_of_couples_squares:
+            print (f'{n} = {pair_of_squares[0]} + {pair_of_squares[1]}')
+    else:
+        print(f'{n}: Nejde to.')
+
+
+if __name__ == '__main__':
+    for a in range(0,1001):
+        sum_of_squares(a)
