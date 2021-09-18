2a94766523e2a8de1cc7569be05e04ef40e3f7db
Michal1898 <michal.volf@gmail.com>
2021-09-18 11:46:03 +0200

    file formatted by black.

--- sum_of_squares.py
+++ sum_of_squares.py
@@ -1,33 +1,32 @@
-
 def sum_of_squares(n):
     list_of_squares = []
-    for side_of_square in range(1,n):
+    for side_of_square in range(1, n):
         square_volume = pow(side_of_square, 2)
-        if (square_volume<n):
+        if square_volume < n:
             list_of_squares.append(square_volume)
         else:
             break
     # couples of volume search
-    list_of_couples_squares=[]
+    list_of_couples_squares = []
     for first_square in list_of_squares:
         for second_square in list_of_squares:
-            if (first_square+second_square)==n:
-                couple_of_squares=(first_square, second_square)
+            if (first_square + second_square) == n:
+                couple_of_squares = (first_square, second_square)
                 list_of_couples_squares.append(couple_of_squares)
 
     if len(list_of_couples_squares):
         for pair_of_squares in list_of_couples_squares:
             # In Pycharm it works fine, but www.umimeprogramovat.cz does not support it! Why?
-            #print (f'{n} = {pair_of_squares[0]} + {pair_of_squares[1]}')
+            # print (f'{n} = {pair_of_squares[0]} + {pair_of_squares[1]}')
 
-            #This is older way and isnot recommended. But www.umimeprogramovat.cz accepted it
-            print(n,'=', pair_of_squares[0], '+', pair_of_squares[1])
+            # This is older way and isnot recommended. But www.umimeprogramovat.cz accepted it
+            print(n, "=", pair_of_squares[0], "+", pair_of_squares[1])
 
     else:
-        print('Nejde to.')
+        print("Nejde to.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sum_of_squares(13)
     print("---")
     sum_of_squares(14)
